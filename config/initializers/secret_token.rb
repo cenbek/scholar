aac8ed9a12005230ce7ad6ea40c8e488b86e1975
Tozkoparan <cetintozkoparan@gmail.com>
2013-02-12 09:24:34 +0200

    Project initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scholar::Application.config.secret_token = '2a674f232a32fa4c91d29e64633ebdf81bdc8154f03f09f901c10a60889820dadbc368078afa4bea1bf8244c767d0438127908a40a1d5300a7a8d4788066d2b9'
